64fe44150f739a69fc0fd8212cdb003300d9cb41
codingpotato <codingpotato@gmail.com>
2020-01-25 13:50:06 +0800

    Sum benchmark

--- main.cpp
+++ main.cpp
@@ -1,17 +1,38 @@
+#include <fstream>
 #include <iostream>
-#include <string>
 
-void run_file(const std::string &) {}
+#include "interpreter.h"
+#include "parser.h"
+#include "scanner.h"
+#include "types.h"
+
+void run(const lox::string &source) {
+  try {
+    lox::scanner scanner{std::move(source)};
+    lox::parser parser{};
+    auto program = parser.parse(scanner.scan());
+    lox::interpreter interpreter{std::cout};
+    interpreter.execute(program);
+  } catch (std::exception &e) {
+    std::cout << e.what() << '\n';
+  }
+}
+
+void run_file(const lox::string &filename) {
+  std::ifstream ifs(filename);
+  run(std::string((std::istreambuf_iterator<char>(ifs)),
+                  (std::istreambuf_iterator<char>())));
+}
 
 void run_prompt() {}
 
 int main(int argc, char *argv[]) {
-  if (argc > 1) {
-    std::cout << "Usage: lox [script]\n";
-    return 1;
+  if (argc == 1) {
+    run_prompt();
   } else if (argc == 2) {
     run_file(argv[1]);
   } else {
-    run_prompt();
+    std::cout << "Usage: lox [script]\n";
   }
+  return 1;
 }
