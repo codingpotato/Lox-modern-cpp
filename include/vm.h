1990e39e7f6401d02bbcc28d1c76fae043f016f5
codingpotato <codingpotato@gmail.com>
2020-04-30 20:28:58 +0800

    optimize VM class

--- include/vm.h
+++ include/vm.h
@@ -23,8 +23,8 @@ class VM {
   explicit VM(std::ostream& os) noexcept
       : out{&os},
         compiler{heap},
-        gc{stack_, heap, globals_, call_frames_, compiler} {
-    register_natives(globals_, heap);
+        gc{stack, heap, globals, call_frames, compiler} {
+    register_natives(globals, heap);
   }
 
   template <bool Debug = false>
@@ -38,22 +38,43 @@ class VM {
  private:
   struct Call_frame {
     Call_frame() noexcept {}
-    Call_frame(Closure* c, size_t start = 0) noexcept
-        : closure{c}, start_of_stack{start} {}
+    Call_frame(Closure* c, size_t bottom = 0) noexcept
+        : closure{c}, bottom_of_stack{bottom} {}
 
     Closure* closure = nullptr;
     size_t ip = 0;
-    size_t start_of_stack = 0;
+    size_t bottom_of_stack = 0;
+  };
+
+  struct Executor {
+    void copy_from(const Closure* closure) noexcept {
+      const auto& chunk = closure->get_func()->get_chunk();
+      code = &chunk.code();
+      constants = &chunk.constants();
+      ip = 0;
+    }
+
+    void copy_from(const Call_frame& frame) noexcept {
+      copy_from(frame.closure);
+      ip = frame.ip;
+    }
+
+    const Bytecode_vector* code = nullptr;
+    const Value_vector* constants = nullptr;
+    size_t ip = 0;
   };
 
   template <typename Func>
   void binary(Func&& func) {
-    auto right = stack_.pop();
-    auto left = stack_.pop();
-    stack_.push(std::forward<Func>(func)(left, right));
+    auto right = stack.pop();
+    auto left = stack.pop();
+    stack.push(std::forward<Func>(func)(left, right));
   }
 
-  Call_frame& current_frame() noexcept { return call_frames_.peek(); }
+  Call_frame& top_frame() noexcept {
+    ENSURES(!call_frames.empty());
+    return call_frames.peek();
+  }
 
   void close_upvalues(const Value* last) noexcept {
     const auto& open_upvalues = heap.get_open_upvalues();
@@ -66,14 +87,11 @@ class VM {
   }
 
   void call_closure(Closure* closure, size_t argument_count) noexcept {
-    if (!call_frames_.empty()) {
-      current_frame().ip = ip;
+    if (!call_frames.empty()) {
+      top_frame().ip = executor.ip;
     }
-    call_frames_.push(closure, stack_.size() - argument_count - 1);
-    const auto& chunk = closure->get_func()->get_chunk();
-    code = &chunk.code();
-    constants = &chunk.constants();
-    ip = 0;
+    call_frames.push(closure, stack.size() - argument_count - 1);
+    executor.copy_from(closure);
   }
 
   void throw_undefined_variable(const String* name) const {
@@ -87,65 +105,63 @@ class VM {
   }
 
   constexpr static size_t max_frame_size = 64;
-  constexpr static size_t max_stack_size = max_frame_size * 1024;
+  constexpr static size_t max_stacksize = max_frame_size * 1024;
 
   std::ostream* out;
   Heap<> heap;
   Compiler compiler;
-  Stack<Call_frame, max_frame_size> call_frames_;
-  Stack<Value, max_stack_size> stack_;
-  Hash_table globals_;
-  const Bytecode_vector* code = nullptr;
-  const Value_vector* constants = nullptr;
-  size_t ip = 0;
-  GC<Stack<Value, max_stack_size>, Stack<Call_frame, max_frame_size>> gc;
+  Stack<Call_frame, max_frame_size> call_frames;
+  Stack<Value, max_stacksize> stack;
+  Hash_table globals;
+  GC<Stack<Value, max_stacksize>, Stack<Call_frame, max_frame_size>> gc;
+  Executor executor;
 };
 
 template <>
 inline void VM::handle(const instruction::Constant& constant) {
-  ENSURES(constants != nullptr);
-  ENSURES(constant.operand() < constants->size());
-  stack_.push((*constants)[constant.operand()]);
+  ENSURES(executor.constants != nullptr);
+  ENSURES(executor.constant.operand() < executor.constants->size());
+  stack.push((*executor.constants)[constant.operand()]);
 }
 
 template <>
 inline void VM::handle(const instruction::Nil&) {
-  stack_.push();
+  stack.push();
 }
 
 template <>
 inline void VM::handle(const instruction::False&) {
-  stack_.push(false);
+  stack.push(false);
 }
 
 template <>
 inline void VM::handle(const instruction::True&) {
-  stack_.push(true);
+  stack.push(true);
 }
 
 template <>
 inline void VM::handle(const instruction::Pop&) {
-  stack_.pop();
+  stack.pop();
 }
 
 template <>
 inline void VM::handle(const instruction::Get_local& get_local) {
-  stack_.push(stack_[current_frame().start_of_stack + get_local.operand()]);
+  stack.push(stack[top_frame().bottom_of_stack + get_local.operand()]);
 }
 
 template <>
 inline void VM::handle(const instruction::Set_local& set_local) {
-  stack_[current_frame().start_of_stack + set_local.operand()] = stack_.peek();
+  stack[top_frame().bottom_of_stack + set_local.operand()] = stack.peek();
 }
 
 template <>
 inline void VM::handle(const instruction::Get_global& get_global) {
-  ENSURES(constants != nullptr);
-  ENSURES(get_global.operand() < constants->size());
-  auto constant = (*constants)[get_global.operand()];
+  ENSURES(executor.constants != nullptr);
+  ENSURES(get_global.operand() < executor.constants->size());
+  auto constant = (*executor.constants)[get_global.operand()];
   auto name = constant.as_object()->as<String>();
-  if (auto value = globals_.get_if(name); value != nullptr) {
-    stack_.push(*value);
+  if (auto value = globals.get_if(name); value != nullptr) {
+    stack.push(*value);
   } else {
     throw_undefined_variable(name);
   }
@@ -153,21 +169,21 @@ inline void VM::handle(const instruction::Get_global& get_global) {
 
 template <>
 inline void VM::handle(const instruction::Define_global& define_global) {
-  ENSURES(constants != nullptr);
-  ENSURES(define_global.operand() < constants->size());
-  auto constant = (*constants)[define_global.operand()];
+  ENSURES(executor.constants != nullptr);
+  ENSURES(define_global.operand() < executor.constants->size());
+  auto constant = (*executor.constants)[define_global.operand()];
   const auto name = constant.as_object()->as<String>();
   const auto str = heap.make_string(name->get_string());
-  globals_.insert(str, stack_.pop());
+  globals.insert(str, stack.pop());
 }
 
 template <>
 inline void VM::handle(const instruction::Set_global& set_global) {
-  ENSURES(constants != nullptr);
-  ENSURES(set_global.operand() < constants->size());
-  auto constant = (*constants)[set_global.operand()];
+  ENSURES(executor.constants != nullptr);
+  ENSURES(set_global.operand() < executor.constants->size());
+  auto constant = (*executor.constants)[set_global.operand()];
   const auto name = constant.as_object()->as<String>();
-  if (!globals_.set(name, stack_.peek())) {
+  if (!globals.set(name, stack.peek())) {
     throw_undefined_variable(name);
   }
 }
@@ -175,15 +191,15 @@ inline void VM::handle(const instruction::Set_global& set_global) {
 template <>
 inline void VM::handle(const instruction::Get_upvalue& get_upvalue) {
   const auto slot = get_upvalue.operand();
-  ENSURES(slot < current_frame().closure->get_upvalues().size());
-  stack_.push(*current_frame().closure->get_upvalues()[slot]->location);
+  ENSURES(slot < top_frame().closure->get_upvalues().size());
+  stack.push(*top_frame().closure->get_upvalues()[slot]->location);
 }
 
 template <>
 inline void VM::handle(const instruction::Set_upvalue& set_upvalue) {
   auto slot = set_upvalue.operand();
-  ENSURES(slot < current_frame().closure->get_upvalues().size());
-  *current_frame().closure->get_upvalues()[slot]->location = stack_.peek();
+  ENSURES(slot < top_frame().closure->get_upvalues().size());
+  *top_frame().closure->get_upvalues()[slot]->location = stack.peek();
 }
 
 template <>
@@ -223,8 +239,8 @@ inline void VM::handle(const instruction::Divide&) {
 
 template <>
 inline void VM::handle(const instruction::Not&) {
-  if (stack_.peek().is_bool()) {
-    stack_.push(!stack_.pop().as_bool());
+  if (stack.peek().is_bool()) {
+    stack.push(!stack.pop().as_bool());
   } else {
     throw runtime_error{"Operand must be a boolean value."};
   }
@@ -232,8 +248,8 @@ inline void VM::handle(const instruction::Not&) {
 
 template <>
 inline void VM::handle(const instruction::Negate&) {
-  if (stack_.peek().is_double()) {
-    stack_.push(-stack_.pop().as_double());
+  if (stack.peek().is_double()) {
+    stack.push(-stack.pop().as_double());
   } else {
     throw runtime_error{"Operand must be a number value."};
   }
@@ -241,21 +257,21 @@ inline void VM::handle(const instruction::Negate&) {
 
 template <>
 inline void VM::handle(const instruction::Print&) {
-  ENSURES(!stack_.empty());
-  *out << to_string(stack_.pop()) << "\n";
+  ENSURES(!stack.empty());
+  *out << to_string(stack.pop()) << "\n";
 }
 
 template <>
 inline void VM::handle(const instruction::Jump& jump) {
-  ip += jump.operand();
+  executor.ip += jump.operand();
 }
 
 template <>
 inline void VM::handle(const instruction::Jump_if_false& jump_if_false) {
-  ENSURES(!stack_.empty());
-  if (auto value = stack_.peek(); value.is_bool()) {
+  ENSURES(!stack.empty());
+  if (auto value = stack.peek(); value.is_bool()) {
     if (!value.as_bool()) {
-      ip += jump_if_false.operand();
+      executor.ip += jump_if_false.operand();
     }
   } else {
     throw runtime_error{"Operand must be a boolean value."};
@@ -264,13 +280,13 @@ inline void VM::handle(const instruction::Jump_if_false& jump_if_false) {
 
 template <>
 inline void VM::handle(const instruction::Loop& loop) {
-  ip -= loop.operand();
+  executor.ip -= loop.operand();
 }
 
 template <>
 inline void VM::handle(const instruction::Call& call) {
   const auto argument_count = call.operand();
-  if (auto v = stack_.peek(argument_count); v.is_object()) {
+  if (auto v = stack.peek(argument_count); v.is_object()) {
     auto obj = v.as_object();
     if (obj->is<Closure>()) {
       auto closure = obj->as<Closure>();
@@ -282,69 +298,64 @@ inline void VM::handle(const instruction::Call& call) {
       }
     } else if (obj->is<Native_func>()) {
       const auto func = obj->as<Native_func>();
-      const auto result =
-          (*func)(argument_count, argument_count > 0
-                                      ? &stack_[stack_.size() - argument_count]
-                                      : nullptr);
-      stack_.resize(stack_.size() - argument_count - 1);
-      stack_.push(result);
+      const auto result = (*func)(
+          argument_count,
+          argument_count > 0 ? &stack[stack.size() - argument_count] : nullptr);
+      stack.resize(stack.size() - argument_count - 1);
+      stack.push(result);
     }
   }
 }
 
 template <>
 inline void VM::handle(const instruction::Closure& closure_instr) {
-  ENSURES(constants != nullptr);
-  ENSURES(closure_instr.operand() < constants->size());
-  auto value = (*constants)[closure_instr.operand()];
+  ENSURES(executor.constants != nullptr);
+  ENSURES(closure_instr.operand() < executor.constants->size());
+  auto value = (*executor.constants)[closure_instr.operand()];
   auto func = value.as_object()->as<Function>();
   auto closure = heap.make_object<Closure>(func);
-  stack_.push(closure);
+  stack.push(closure);
   const auto upvalues = closure_instr.upvalues();
   for (size_t i = 0; i < func->upvalue_count; ++i) {
     auto is_local = upvalues[i * 2];
     auto index = upvalues[i * 2 + 1];
     if (is_local) {
-      auto value = &stack_[current_frame().start_of_stack + index];
+      auto value = &stack[top_frame().bottom_of_stack + index];
       closure->get_upvalues()[i] = heap.make_upvalue(value);
     } else {
-      closure->get_upvalues()[i] =
-          current_frame().closure->get_upvalues()[index];
+      closure->get_upvalues()[i] = top_frame().closure->get_upvalues()[index];
     }
   }
-  ip += func->upvalue_count * 2;
+  executor.ip += func->upvalue_count * 2;
 }
 
 template <>
 inline void VM::handle(const instruction::Close_upvalue&) {
-  close_upvalues(&stack_.back());
-  stack_.pop();
+  close_upvalues(&stack.back());
+  stack.pop();
 }
 
 template <>
 inline void VM::handle(const instruction::Return&) {
-  auto result = stack_.pop();
-  auto stack_size = current_frame().start_of_stack;
-  close_upvalues(&stack_[current_frame().start_of_stack]);
-  call_frames_.pop();
-  if (call_frames_.empty()) {
-    stack_.pop();
+  auto result = stack.pop();
+  auto stacksize = top_frame().bottom_of_stack;
+  close_upvalues(&stack[top_frame().bottom_of_stack]);
+  call_frames.pop();
+  if (call_frames.empty()) {
+    stack.pop();
   } else {
-    stack_.resize(stack_size);
-    stack_.push(result);
-    const auto& chunk = current_frame().closure->get_func()->get_chunk();
-    code = &chunk.code();
-    constants = &chunk.constants();
-    ip = current_frame().ip;
+    stack.resize(stacksize);
+    stack.push(result);
+    executor.copy_from(top_frame());
   }
 }
 
-#define INTERPRET_CASE(instr_struct, base)                \
-  case instruction::instr_struct::opcode: {               \
-    auto instr = instruction::instr_struct{&(*code)[ip]}; \
-    ip += instr.size;                                     \
-    handle(instr);                                        \
-    break;                                                \
+#define INTERPRET_CASE(instr_struct, base)                                  \
+  case instruction::instr_struct::opcode: {                                 \
+    auto instr = instruction::instr_struct{&(*executor.code)[executor.ip]}; \
+    executor.ip += instr.size;                                              \
+    handle(instr);                                                          \
+    break;                                                                  \
   }
 
 template <bool Debug>
@@ -353,21 +364,21 @@ inline void VM::interpret(std::string source) noexcept {
     lox::scanner scanner{std::move(source)};
     auto func = compiler.compile(scanner.scan());
     auto closure = heap.make_object<Closure>(func);
-    stack_.push(closure);
+    stack.push(closure);
     call_closure(closure, 0);
-    while (ip < code->size()) {
-      switch ((*code)[ip]) { INSTRUCTIONS(INTERPRET_CASE) }
+    while (executor.ip < executor.code->size()) {
+      switch ((*executor.code)[executor.ip]) { INSTRUCTIONS(INTERPRET_CASE) }
       if constexpr (Debug) {
-        for (size_t i = 0; i < stack_.size(); ++i) {
-          *out << to_string(stack_[i]) << " ";
+        for (size_t i = 0; i < stack.size(); ++i) {
+          *out << to_string(stack[i]) << " ";
         }
         *out << "\n";
       }
     }
   } catch (exception& e) {
     *out << e.what() << "\n";
-    for (size_t distance = 0; distance < call_frames_.size(); ++distance) {
-      auto& frame = call_frames_.peek(distance);
+    for (size_t distance = 0; distance < call_frames.size(); ++distance) {
+      auto& frame = call_frames.peek(distance);
       auto func = frame.closure->get_func();
       *out << "[line " << std::setfill('0') << std::setw(4)
            << func->get_chunk().lines()[frame.ip] << " in] "
