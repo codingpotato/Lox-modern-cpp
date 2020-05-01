9b6b41736f7b9afc6985fe94685e8f55e6856581
codingpotato <codingpotato@gmail.com>
2020-05-01 22:39:42 +0800

    String tests

--- tests/scanner_tests.cpp
+++ tests/scanner_tests.cpp
@@ -14,25 +14,58 @@ inline void check_tokens(const lox::Token_vector& tokens,
   }
 }
 
-TEST_CASE("scanner: print") {
-  lox::Scanner scanner{"print 1 + 2;"};
+TEST_CASE("scanner: identifiers") {
+  lox::Scanner scanner{R"(
+andy formless fo _ _123 _abc ab123
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_
+)"};
   auto tokens = scanner.scan();
   std::vector<lox::Token> expected = {
-      {lox::Token::k_print, "print", 1}, {lox::Token::number, "1", 1},
-      {lox::Token::plus, "+", 1},        {lox::Token::number, "2", 1},
-      {lox::Token::semicolon, ";", 1},   {lox::Token::eof, "", 1},
+      {lox::Token::identifier, "andy", 2},
+      {lox::Token::identifier, "formless", 2},
+      {lox::Token::identifier, "fo", 2},
+      {lox::Token::identifier, "_", 2},
+      {lox::Token::identifier, "_123", 2},
+      {lox::Token::identifier, "_abc", 2},
+      {lox::Token::identifier, "ab123", 2},
+      {lox::Token::identifier,
+       "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_", 3},
+      {lox::Token::eof, "", 4},
   };
   check_tokens(tokens, expected);
 }
 
-TEST_CASE("scanner: if") {
-  lox::Scanner scanner{"if (true) print 1;"};
+TEST_CASE("scanner: keywords") {
+  lox::Scanner scanner{R"(
+and class else false for fun if nil or return super this true var while
+)"};
   auto tokens = scanner.scan();
   std::vector<lox::Token> expected = {
-      {lox::Token::k_if, "if", 1},       {lox::Token::left_paren, "(", 1},
-      {lox::Token::k_true, "true", 1},   {lox::Token::right_paren, ")", 1},
-      {lox::Token::k_print, "print", 1}, {lox::Token::number, "1", 1},
-      {lox::Token::semicolon, ";", 1},   {lox::Token::eof, "", 1},
+      {lox::Token::k_and, "and", 2},     {lox::Token::k_class, "class", 2},
+      {lox::Token::k_else, "else", 2},   {lox::Token::k_false, "false", 2},
+      {lox::Token::k_for, "for", 2},     {lox::Token::k_func, "fun", 2},
+      {lox::Token::k_if, "if", 2},       {lox::Token::k_nil, "nil", 2},
+      {lox::Token::k_or, "or", 2},       {lox::Token::k_return, "return", 2},
+      {lox::Token::k_super, "super", 2}, {lox::Token::k_this, "this", 2},
+      {lox::Token::k_true, "true", 2},   {lox::Token::k_var, "var", 2},
+      {lox::Token::k_while, "while", 2}, {lox::Token::eof, "", 3},
+  };
+  check_tokens(tokens, expected);
+}
+
+TEST_CASE("scanner: numbers") {
+  lox::Scanner scanner{R"(
+123
+123.456
+.456
+123.
+)"};
+  auto tokens = scanner.scan();
+  std::vector<lox::Token> expected = {
+      {lox::Token::number, "123", 2}, {lox::Token::number, "123.456", 3},
+      {lox::Token::dot, ".", 4},      {lox::Token::number, "456", 4},
+      {lox::Token::number, "123", 5}, {lox::Token::dot, ".", 5},
+      {lox::Token::eof, "", 6},
   };
   check_tokens(tokens, expected);
 }
---
+++ tests/string_tests.cpp
@@ -0,0 +1,55 @@
+#include <doctest/doctest.h>
+
+#include "helper.h"
+
+TEST_CASE("string: error after multiline") {
+  const std::string source{R"(
+var a = "1
+2
+3
+";
+err;
+)"};
+  const std::string expected{R"(Undefined variable: 'err'.
+[line 0005] in <script>
+)"};
+  CHECK_EQ(run(source), expected);
+}
+
+TEST_CASE("string: literals") {
+  const std::string source{R"raw(
+print "(" + "" + ")";
+print "a string";
+print "A~¶Þॐஃ";
+)raw"};
+  const std::string expected{R"(()
+a string
+A~¶Þॐஃ
+)"};
+  CHECK_EQ(run(source), expected);
+}
+
+TEST_CASE("string: multiline") {
+  const std::string source{R"(
+var a = "1
+2
+3";
+print a;
+)"};
+  const std::string expected{R"(1
+2
+3
+)"};
+  CHECK_EQ(run(source), expected);
+}
+
+TEST_CASE("string: unterminated") {
+  const std::string source{R"(
+"this string has no close quote
+)"};
+  const std::string expected{
+      R"([line 3] Error at '"this string has no close quote
+': Unterminated string.
+)"};
+  CHECK_EQ(run(source), expected);
+}
